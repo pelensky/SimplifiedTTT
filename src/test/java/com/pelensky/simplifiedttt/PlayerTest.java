ae0c4a3b840c376a7701ab11f48457d8185620c5
Dan Pelensky <dan@pelensky.com>
2017-06-06 17:00:34 +0100

    Computer player gets a marker

--- src/test/java/com/pelensky/simplifiedttt/PlayerTest.java
+++ src/test/java/com/pelensky/simplifiedttt/PlayerTest.java
@@ -8,15 +8,18 @@ import static org.junit.Assert.assertEquals;
 public class PlayerTest {
 
     private Player human;
+    private Player computer;
 
     @Before
     public void setUp() {
         human = new HumanPlayer("X");
+        computer = new ComputerPlayer("O");
     }
 
     @Test
     public void playerInitializedWithAMarker() {
         assertEquals("X", human.getMarker());
+        assertEquals("O", computer.getMarker());
     }
 
 }
