56dedf5123625ee619255f28944358482318447d
Dan Pelensky <dan@pelensky.com>
2017-06-06 17:28:02 +0100

    Game constructor created

--- src/main/java/com/pelensky/simplifiedttt/Game.java
+++ src/main/java/com/pelensky/simplifiedttt/Game.java
@@ -1,5 +1,14 @@
 package com.pelensky.simplifiedttt;
 
-public class Game {
+class Game {
 
+    private final Board board;
+    private final Player player1;
+    private final Player player2;
+
+    Game(Board board, Player player1, Player player2) {
+        this.board = board;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
 }
--- src/test/java/com/pelensky/simplifiedttt/GameTest.java
+++ src/test/java/com/pelensky/simplifiedttt/GameTest.java
@@ -1,5 +1,20 @@
 package com.pelensky.simplifiedttt;
 
+import org.junit.Before;
+
 public class GameTest {
+
+    private Game game;
+    private Board board;
+    private Player player1;
+    private Player player2;
+
+    @Before
+    public void setUp() {
+        board = new Board(3);
+        player1 = new HumanPlayer("X");
+        player2 = new HumanPlayer("O");
+        game = new Game(board, player1, player2);
+    }
 
 }
